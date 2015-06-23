986c501c559e83e6f7995092fe6f65e5ded4d7a2
pamela Adams <pwadams@me.com>
2015-06-23 17:14:13 -0600

    fixing background image to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.serve_static_assets = true
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
@@ -53,7 +55,6 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
